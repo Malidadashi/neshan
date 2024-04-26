dd9752dcc38d034fd8da0f85617e14e103a948d1
M.Ali Dadashi <122849217+Malidadashi@users.noreply.github.com>
2024-04-26 23:09:55 +0330

    6th commit

--- src/Components/Body1/Body.jsx
+++ src/Components/Body1/Body.jsx
@@ -4,15 +4,11 @@ import miladtower from "../../assets/miladtower.jpg";
 const Body = () => {
   return (
     <div className="post">
-      <img className="miladtower" src={miladtower} alt="asd" />
-     
-      <div className="a">
-        <h1>
-          <div className="text-1">با هم</div>
-          <div className="text-2">بهترین مسیر رو</div>
-          <div className="text3">پیدا می‌کنیم</div>
-        </h1>
-      </div>
+      <img className="post-miladtower" src={miladtower} alt="asd" />
+      <h1 className="post-text">
+باهم بهترین مسیر رو<br/>
+پیدا می کنیم
+      </h1>
     </div>
   );
 };
--- src/Components/Button1/Button1.jsx
+++ src/Components/Button1/Button1.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import './button1.css'
-import btn1 from '../../assets/btn1.png'
+import React from "react";
+import "./button1.css";
+import btn1 from "../../assets/btn1.png";
 import { TiDownload } from "react-icons/ti";
 const Button1 = () => {
   return (
-    <div>
-<img className='navpic' src={btn1} alt='#'/>
-<h1>نشان چه کاری انجام میده؟</h1>   
-<p>نقشه و مسیر یاب نشان
-    یک اپلیکیشن بسیار کاربردی و رایگان است که
-     می تواند به وسیله آن بهترین راه رسیدن
-به مقصد را با توجه به مسافت و ترافیک بدست بیاورید و تا رسیدن به مقصد از راهنمایی سخنگوی فارسی استفاده کنید
-</p>
+    <div className="total">
+      <img className="total-img" src={btn1} alt="#" />
+      <h1 className="total-h1">نشان چه کاری انجام میده؟</h1>
+      <p total-text>
+        <p>
+          نقشه و مسیریاب نشان» یک اپلیکیشن رایگان و کاربردی است که می‌توانید به
+          وسیله آن»{" "}
+        </p>
+        <p>
+          بهترین راه رسیدن به مقصد را با توجه به مسافت و ترافیک بدست بیاورید و
+          تا رسیدن به
+        </p>
+        <p>مقصد از راهنمایی سخن‌گوی فارسی استفاده کنید</p>
+      </p>
+      <button className="total-btn">
+        دانلود نشان
+        <TiDownload />
+      </button>
+    </div>
+  );
+};
 
-<button>دانلود نشان<TiDownload/></button>
-</div>
-  )
-}
-
-export default Button1
+export default Button1;
