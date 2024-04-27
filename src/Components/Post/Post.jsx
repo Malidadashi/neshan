7f837718d0e53efc983ff5adb8c79bd82897e1dd
M.Ali Dadashi <122849217+Malidadashi@users.noreply.github.com>
2024-04-27 21:43:39 +0330

    7th commit

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -10,25 +10,26 @@ import { FaLessThan } from "react-icons/fa";
 const Post = () => {
   return (
     <div className="graybox">
-      <div className="input">
-        <h2>دنبال هرچی می‌گردی همین حالا در نشــان پیداش کن.</h2>
+      <div className="graybox-h2">
+        <h2>دنبال هرچی می‌گردی همین حالا در نشــان پیداش کن .</h2>
+      </div>
+      <div className="graybox-input">
         <input placeholder="رستوران،مراکز درمانی،مراکز تفریحی"></input>
         <IoMdSearch />
+      </div>
 
-        <div className="icons">
-          <BsBasket />
-          <MdOutlineAtm />
-          <FaHospitalSymbol />
-          <RiRestaurant2Fill />
-        </div>
-        <div>
-          <button>
-            نقشه شهر های ایران
-            <FaLessThan />
-          </button>
-        </div>
+      <div className="graybox-icons">
+        <BsBasket />
+        <MdOutlineAtm />
+        <FaHospitalSymbol />
+        <RiRestaurant2Fill />
+      </div>
 
-
+      <div className="graybox-button">
+        <button>
+          نقشه شهر های ایران
+          <FaLessThan />
+        </button>
       </div>
     </div>
   );
