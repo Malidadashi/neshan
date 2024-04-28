fe586b8403d5babecf948eb73ef074f4819ac6c1
mad <mad@yahoo.com>
2024-04-28 12:04:44 +0330

    8th

--- src/Components/Dev/Dev.jsx
+++ src/Components/Dev/Dev.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from "react";
 import { FaLessThan } from "react-icons/fa";
-import './dev.css'
+import magicbox from '../../assets/magicbox.png'
+import '../../assets/shakehand.png'
+import "./dev.css";
 
 const Dev = () => {
   return (
-    <div>
-    <div>
-<h4>توسعه دهندگان</h4>
-<p>ِبیشتر<FaLessThan /></p>
-</div>
-    <div>
-    <h4>ره کار های سازمانی</h4>
-<p>ِبیشتر<FaLessThan /></p>
+    <div className="box">
+      <img src={magicbox} alt="#"/>
+      <h4>توسعه دهندگان</h4>
+      <p>
+        ِبیشتر
+        <FaLessThan />
+      </p>
+      <h4>ره کار های سازمانی</h4>
+      <p>
+        ِبیشتر
+        <FaLessThan />
+      </p>
     </div>
-</div>
-  )
-}
+  );
+};
 
-export default Dev
+export default Dev;
